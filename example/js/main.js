46df7b624c0ba8f4ef520e2ba8199cbe018d5f5c
Victor Cabieles <victorcabieles@gmail.com>
2018-01-26 20:23:07 -0500

    Changed css adde methods to mainjs

--- example/js/main.js
+++ example/js/main.js
@@ -1,15 +1,21 @@
-(function () {
+// (function () {
     var camera, scene, renderer;
     var geometry, material, mesh;
+    var arenaDom = {
+        element: document.getElementById("arena"),
+        container: document.getElementById("canvasContainer"),
+        width: function (){ return this.container.clientWidth},
+        height: function (){ return this.container.clientHeight}
+    };
 
     init();
     animate();
 
     function init() {
 
-        camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+        // #Camera & Scene
+        camera = new THREE.PerspectiveCamera( 70, arenaDom.width() / arenaDom.height(), 0.01, 10 );
         camera.position.z = 1;
-
         scene = new THREE.Scene();
 
         geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
@@ -18,10 +24,8 @@
         mesh = new THREE.Mesh( geometry, material );
         scene.add( mesh );
 
-        renderer = new THREE.WebGLRenderer( { antialias: true } );
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        document.body.appendChild( renderer.domElement );
-
+        renderer = new THREE.WebGLRenderer( { antialias: true, canvas: arenaDom.element } );
+        renderer.setSize( arenaDom.width(), arenaDom.height() );
     }
 
     function animate() {
@@ -34,4 +38,4 @@
         renderer.render( scene, camera );
 
     }
-}());
+// }());
